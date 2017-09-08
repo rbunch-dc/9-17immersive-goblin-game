97a545d12009ed3421602db4ba0514c1f293fc63
Robert Bunch <rob@digitalcrafts.com>
2017-09-08 15:48:53 -0400

    Add goblion and collision

--- game.py
+++ game.py
@@ -2,6 +2,9 @@
 # Include pygame which we got from pip
 import pygame
 
+# from the math module (built into python), get the fabs method
+from math import fabs
+
 # 2. Init pygame
 # in order to use pygame, we have to run the init method
 pygame.init()
@@ -16,12 +19,20 @@ pygame.display.set_caption("Goblin Chase")
 # set up a var with our image
 background_image = pygame.image.load('background.png')
 hero_image = pygame.image.load('hero.png')
+goblin_image = pygame.image.load('goblin.png')
 
 # 8. Set up the hero location
 hero = {
 	"x": 100,
 	"y": 100,
-	"speed": 20
+	"speed": 20,
+	"wins": 0
+}
+
+goblin = {
+	"x": 200,
+	"y": 200,
+	"speed": 15
 }
 
 keys = {
@@ -65,6 +76,18 @@ while game_on:
 			elif event.key == keys['right']:
 				# hero['x'] += hero['speed']
 				keys_down['right'] = True
+		elif event.type == pygame.KEYUP:
+			# the user let go of a key. See if it's one that matters
+			if event.key == keys['up']:
+				# user let go of the upkey. Flip the bool
+				keys_down['up'] = False
+			if event.key == keys['down']:
+				keys_down['down'] = False
+			if event.key == keys['right']:
+				keys_down['right'] = False
+			if event.key == keys['left']:
+				keys_down['left'] = False
+
 
 	if keys_down['up']:
 		hero['y'] -= hero['speed']
@@ -75,13 +98,28 @@ while game_on:
 	elif keys_down['right']:
 		hero['x'] += hero['speed']
 
+	# COLLISION DETECTION!!!
+	distance_between = fabs(hero['x'] - goblin['x']) + fabs(hero['y'] - goblin['y'])
+	if distance_between < 32:
+		# the hero and goblin are touching!
+		print "collision!"
+	else:
+		print "not touching"
+
 	# 6. Fill in the screen with a color (or image)
 	# ACTUALLY RENDER SOMETHING
 	# blit takes 2 arguments...
 	# 1. What do you want to draw?
 	# 2. Where do you watn you to draw it
 	pygame_screen.blit(background_image, [0,0])
+
+	# Make a font so we can write on the screen
+	font = pygame.font.Font(None, 25)
+	wins_text = font.render("Wins: %d" % (hero['wins']), True, (0,0,0))
+	pygame_screen.blit(wins_text,[40,40])
+	
 	pygame_screen.blit(hero_image, [hero['x'],hero['y']])
+	pygame_screen.blit(goblin_image, [goblin['x'],goblin['y']])
 
 	# 7. Repeat 6 over and over over...
 	pygame.display.flip()
